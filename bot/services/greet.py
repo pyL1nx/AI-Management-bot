74a82bc8ba5318e13d81e47ea985ef60b8375736
𝗔ᴍʙʀᴏsᴇ <ambrose.linux+github@gmail.com>
2025-09-02 20:17:03 +0530

    Update greet.py

--- bot/services/greet.py
+++ bot/services/greet.py
@@ -2,5 +2,5 @@ from aiogram import types
 from ..utils.constants import GREETING_TEXT
 
 async def greet_user(message: types.Message, target: types.User):
-    mention = target.mention_html(target.full_name)
+    mention = target.mention_html(target.full_name)  # safe HTML entity from Telegram API
     await message.reply(GREETING_TEXT.format(mention=mention))
